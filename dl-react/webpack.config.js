48cc46c60b879f00639127134a391cc1d01f2ebd
harddog <867129306@qq.com>
2018-09-17 00:23:59 +0800

    解决webpack 自定义输出文件夹，输出html没有root等问题

--- dl-react/webpack.config.js
+++ dl-react/webpack.config.js
@@ -2,6 +2,11 @@
 // - https://github.com/ant-tool/atool-build#配置扩展
 
 var webpack = require('atool-build/lib/webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ReactRootPlugin = require('html-webpack-react-root-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const merge = require('webpack-merge');
+const path = require('path');
 
 module.exports = function(webpackConfig){
     webpackConfig.babel.plugins.push('transform-runtime');
@@ -10,22 +15,25 @@ module.exports = function(webpackConfig){
         style: 'css',
     }]);
 
-    webpackConfig.devServer = {
-        proxy: {
-            '/api/**': {
-                target: 'http://localhost:5000',
-                secure: false
-                // changeOrigin: true
-            },
-        }
-    };
-
     webpackConfig.plugins.push(
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'pp助手-越狱App下载',
+            filename: '../templates/index.html'
+        }),
+        new ReactRootPlugin(),
         new webpack.DefinePlugin({
-            __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || 'true')),
-            __PRERELEASE__: JSON.stringify(JSON.parse(process.env.BUILD_PRERELEASE || 'false'))
-        })
+            __DEV__: JSON.stringify('true')
+        }),
     );
 
-    return webpackConfig;
+    appExport = {
+        devServer: {
+            proxy: "http://localhost:5000"
+        },
+        output: {
+            path: path.resolve(__dirname, './dist/static/')
+        }
+    };
+    return merge(webpackConfig, appExport);
 };
